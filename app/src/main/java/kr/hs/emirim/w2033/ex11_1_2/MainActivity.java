82f0199549106dbf2839d9523f25906c5807b3f5
Song <w2033@e-mirim.hs.kr>
2021-07-26 11:47:57 +0900

    Edit MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2033/ex11_1_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2033/ex11_1_2/MainActivity.java
@@ -3,12 +3,31 @@ package kr.hs.emirim.w2033.ex11_1_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    String[] arr = {"귀멸의 칼날", "하이큐","약속의 네버랜드","강철의 연금술사","코드기어스","명탐정 코난","기븐"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list1 = findViewById(R.id.list1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arr);
+        list1.setAdapter(adapter);
+
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),arr[i],Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
